bb0b19528c87aec793715cffba1fbd348ed87494
SOUMEN-PAL <112452467+SOUMEN-PAL@users.noreply.github.com>
2023-01-14 16:11:20 +0530

    Count set bits Brian keringum's equation

--- learn/BitWise/COunt_setBIT.cpp
+++ learn/BitWise/COunt_setBIT.cpp
@@ -1,22 +1,39 @@
 #include <iostream>
 using namespace std;
 
-int Countbit(int n){
-    int set=0;
-    while(n>0){
-        if(n&1!=0){
-            set=set+1;
+int Countbit(int n)
+{
+    int set = 0;
+    while (n > 0)
+    {
+        if (n & 1 != 0)
+        {
+            set = set + 1;
         }
-        n=n>>1;
+        n = n >> 1;
     }
     return set;
 }
+// Efficient solution Time complexity: O(set bits) //
 
-int main(){
-    int x=Countbit(10);
-    cout<<x<<endl;
-    
+int countSETbits(int n)
+{
+    int res = 0;
+    while (n > 0)
+    {
+        n = n & (n - 1);// <----- Brian Keringum's Equation //
+        res++;
+    }
+    return res;
+}
 
+// efficient solution Lookup table //
+
+int main()
+{
+    int x = Countbit(32);
+    int y = countSETbits(32);
+    cout << x << " " << y << endl;
 
     return 0;
 }

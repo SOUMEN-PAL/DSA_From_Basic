ff5a341dba4793497875a70c0fda436dc7babb9b
SOUMEN-PAL <112452467+SOUMEN-PAL@users.noreply.github.com>
2023-01-30 14:22:06 +0530

    Josephus Solution and explanation

---
+++ Rough/rough_rec.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+unsigned int foo(unsigned int n , unsigned int r){
+
+    if(n>0){return (n%r +foo(n/r , r));}
+
+    else{
+        return 0;
+    }
+}
+
+int main(){
+    cout<<foo(513,2);
+
+    return 0;
+}
---
+++ learn/Recurssion/Josephus.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int jos(int n , int k ){
+    if(n==1){
+        return 0;
+    }
+    else{
+        return (jos(n-1 , k)+k)%n;
+    }
+}
+
+
+int main(){
+    int N , k;
+    cout<<"Enter vlaue of N: ";
+    cin>>N;
+
+    cout<<"Sequence to kill: ";
+    cin>>k;
+
+    int res = jos(N , k);
+    cout<<"The person alive is: "<<res+1<<endl;
+
+
+    return 0;
+}

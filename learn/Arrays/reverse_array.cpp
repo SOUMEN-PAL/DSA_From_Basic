a6e85c8d5d9ef1f9a5fbe72ba095c57ae96feb22
SOUMEN-PAL <112452467+SOUMEN-PAL@users.noreply.github.com>
2023-02-10 20:47:19 +0530

    Reverse of an array

--- learn/Arrays/reverse_array.cpp
+++ learn/Arrays/reverse_array.cpp
@@ -1,21 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// my solution //
+
 void rev(int arr[] , int n){
     int m=n-1;
-    for(int i = 0 ; i<n ; i++){
-        if(i<m){
-            arr[i] = arr[m];
-        }
-        else{
-            arr[m]=arr[i];
-        }
+    for(int i = 0 ; i<n/2 ; i++){
+        swap(arr[m],arr[i]);
         m=m-1;
     }
 
     for(int j = 0 ; j<n ; j++){
         cout<<arr[j]<<" ";
     }
+    cout<<endl;
+}
+
+
+//naive solution //
+
+void rev_arr(int arr[],int n){
+    int low = 0 , high = n-1;
+
+    while(low<high){
+        int temp = arr[low];
+        arr[low] = arr[high];
+        arr[high] = temp;
+
+
+        low++;
+        high--;
+    }
+    for(int j = 0 ; j<n ; j++){
+        cout<<arr[j]<<" ";
+    }
+
+    cout<<endl;
+
+
+
+
 }
 
 
@@ -24,7 +48,18 @@ int main(){
     int arr[]={1,2,3,4,5};
     int n = sizeof(arr)/sizeof(arr[0]);
     rev(arr,n);
+    rev_arr(arr,n);
 
     
     return 0;
 }
+
+/*
+
+5 4 3 2 1--> reversed the array 
+1 2 3 4 5--> again called it is reversed
+
+as array is alredy is a direct acess to the memory
+and the changes will effect directly.
+
+*/

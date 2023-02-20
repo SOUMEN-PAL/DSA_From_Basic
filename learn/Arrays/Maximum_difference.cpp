098dd957b8022524196c2b4e2d334ea8fe5b04c3
SOUMEN-PAL <soumenpal3241@gmail.com>
2023-02-21 01:03:21 +0530

    Maximum difference

--- learn/Arrays/Maximum_difference.cpp
+++ learn/Arrays/Maximum_difference.cpp
@@ -4,9 +4,9 @@ using namespace std;
 // naive solution //
 
 void diff(int arr[] , int n){
-    int temp;
+    int temp = arr[1] - arr[0];
     for(int i = 0 ; i<n ; i++){
-        for(int j = i+1 ; j<n ; j++){
+        for(int j = n-1 ; j>i ; j--){
             temp = max(temp , (arr[j]-arr[i]));
         }
     }
@@ -16,13 +16,22 @@ void diff(int arr[] , int n){
 
 // efficient solution //
 
+void max_diff(int arr[] , int n){
+    int temp = arr[1]-arr[0] , minvalue = arr[0];
+    for(int i = 0 ; i<n ; i++){
+        temp = max(temp , arr[i]-minvalue);
+        minvalue = min(arr[i],minvalue);
+    }
 
+    cout<<temp<<endl;
+}
 
 
 int main(){
     int arr[] = {30,10,8,2};
     int n = sizeof(arr)/sizeof(arr[0]);
     diff(arr , n);
+    max_diff(arr , n);
     
 
 

ba4f0490e4a4c23ae1142cf3b59f32bdd85fefac
SOUMEN-PAL <soumenpal3241@gmail.com>
2023-02-21 22:13:46 +0530

    Maximum differnece in an array  efficient

--- learn/Arrays/Leader_in_arr.cpp
+++ learn/Arrays/Leader_in_arr.cpp
@@ -1,63 +1,62 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 // naiv solution //
-void lead(int arr[] , int n){
-	for(int i = 0; i < n; i++)
+void lead(int arr[], int n)
+{
+	for (int i = 0; i < n; i++)
 	{
 		bool flag = false;
 
-		for(int j = i + 1; j < n; j++)
+		for (int j = i + 1; j < n; j++)
 		{
-			if(arr[i] <= arr[j])
+			if (arr[i] <= arr[j])
 			{
 				flag = true;
 				break;
 			}
 		}
 
-		if(flag == false)
+		if (flag == false)
 		{
-			cout<<arr[i]<<" ";
+			cout << arr[i] << " ";
 		}
 	}
-	cout<<endl;
-
-
+	cout << endl;
 }
 
 // efficient solution //
 
-vector<int> leader(int arr[] , int n){
+vector<int> leader(int arr[], int n)
+{
 	vector<int> ans;
-	int curr_lead = arr[n-1];
+	int curr_lead = arr[n - 1];
 	// we know that the last element is always a leader //
 	ans.push_back(curr_lead);
-	for(int i = n-2 ; i>=0 ; i--){
-		if(arr[i]>curr_lead){
+	for (int i = n - 2; i >= 0; i--)
+	{
+		if (arr[i] > curr_lead)
+		{
 			curr_lead = arr[i]; // updating the value of current lead //
 			ans.push_back(curr_lead);
-
 		}
 	}
 	return ans;
-// TC : O(n)
-// AS : O(1)
+	// TC : O(n)
+	// AS : O(1)
 }
 
+int main()
+{
+	int arr[] = {7, 10, 4, 3, 6, 5, 2};
+	int n = sizeof(arr) / sizeof(arr[0]);
 
-
-
-int main(){
-    int arr[] = {7,10,4,3,6,5,2};
-    int n = sizeof(arr)/sizeof(arr[0]);
-
-    lead(arr, n);
-	vector<int> sol = leader(arr , n);
-	for(int i  = 0 ; i<sol.size() ; i++){
-		cout<<sol[i]<<" ";
+	lead(arr, n);
+	vector<int> sol = leader(arr, n);
+	for (int i = 0; i < sol.size(); i++)
+	{
+		cout << sol[i] << " ";
 	}
-    
-    return 0;
+
+	return 0;
 }
--- learn/Arrays/Maximum_difference.cpp
+++ learn/Arrays/Maximum_difference.cpp
@@ -3,37 +3,40 @@ using namespace std;
 
 // naive solution //
 
-void diff(int arr[] , int n){
+void diff(int arr[], int n)
+{
     int temp = arr[1] - arr[0];
-    for(int i = 0 ; i<n ; i++){
-        for(int j = n-1 ; j>i ; j--){
-            temp = max(temp , (arr[j]-arr[i]));
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = n - 1; j > i; j--)
+        {
+            temp = max(temp, (arr[j] - arr[i]));
         }
     }
 
-    cout<<temp<<endl;
+    cout << temp << endl;
 }
 
 // efficient solution //
 
-void max_diff(int arr[] , int n){
-    int temp = arr[1]-arr[0] , minvalue = arr[0];
-    for(int i = 0 ; i<n ; i++){
-        temp = max(temp , arr[i]-minvalue);
-        minvalue = min(arr[i],minvalue);
+void max_diff(int arr[], int n)
+{
+    int temp = arr[1] - arr[0], minvalue = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        temp = max(temp, arr[i] - minvalue);
+        minvalue = min(arr[i], minvalue);
     }
 
-    cout<<temp<<endl;
+    cout << temp << endl;
 }
 
-
-int main(){
-    int arr[] = {30,10,8,2};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    diff(arr , n);
-    max_diff(arr , n);
-    
-
+int main()
+{
+    int arr[] = {30, 10, 8, 2};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    diff(arr, n);
+    max_diff(arr, n);
 
     return 0;
 }

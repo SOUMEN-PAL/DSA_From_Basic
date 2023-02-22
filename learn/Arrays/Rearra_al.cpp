977c1f2d03a9864a3235e61b649aa00e9272d515
SOUMEN-PAL <soumenpal3241@gmail.com>
2023-02-22 21:47:03 +0530

    Rearrange alternate efficient solution

--- learn/Arrays/Rearra_al.cpp
+++ learn/Arrays/Rearra_al.cpp
@@ -19,16 +19,42 @@ void alter(int arr[] , int n){
         rotate(arr , n , i);
     }
 }
+// TC: O(n^2)
+// AS : O(1);
 
 
+// efficinet solution //
 
+void rearr(int arr[] , int n){
+    int max = n-1; // two pointers approach //
+    int min = 0;
+    int max_ele = arr[n-1]+1;
+    for(int i = 0 ; i<n ; i++){
+        if(i%2==0){
+            arr[i] = arr[i] + (arr[max]%max_ele)*max_ele;
+            max--;
+        }
+        else{
+            arr[i] = arr[i] + (arr[min]%max_ele)*max_ele;
+            min++;
+        }
+    }
+
+    for(int i = 0 ; i<n ; i++){
+        arr[i] = arr[i]/max_ele;
+    }
+
+}
+
+// TC : O(n);
+// AS : O(1);
 
 
 int main(){
     int arr[] = {1,2,3,4,5,6};
     int n = sizeof(arr)/sizeof(arr[0]);
 
-    alter(arr , n);
+    rearr(arr , n);
 
     for(int i = 0 ; i<n ; i++){
         cout<<arr[i]<<" ";

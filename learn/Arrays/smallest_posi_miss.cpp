108d4edd8bebaef8bc69c186d1f2154b6a28fb43
SOUMEN-PAL <soumenpal3241@gmail.com>
2023-02-25 17:45:44 +0530

    rough

--- learn/Arrays/smallest_posi_miss.cpp
+++ learn/Arrays/smallest_posi_miss.cpp
@@ -1,8 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-int indexi(int arr [], int n){
+int incre(int arr[] , int n){
     for(int i = 0 ; i<n ; i++){
         if(arr[i]>0){
             return i;
@@ -10,27 +9,15 @@ int indexi(int arr [], int n){
         }
     }
 }
-int missingNumber(int arr[] , int n){
-    sort(arr,arr+n);
-    int ind = indexi(arr , n);
-    int i =1;
-    while(i<n){
-        if(arr[ind]!=i){
-            return i;
-        }
-        
-        return i+1;
-        
-        i++;
-        ind++;
-    }
-}
+
+
+
 
 int main(){
     
-    int arr[] = {1 ,2 ,3 ,4 ,5};
+    int arr[] = {0,1 ,2 ,3 ,4 ,5};
     int n = sizeof(arr) / sizeof(arr[0]);
-    cout<<missingNumber(arr,n);
+    cout<<incre(arr , n);
 
     return 0;
 }

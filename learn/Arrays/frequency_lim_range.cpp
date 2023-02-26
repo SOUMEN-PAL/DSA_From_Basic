3d4da61d05c36c516f5911a37b3d5bb8f608b326
SOUMEN-PAL <soumenpal3241@gmail.com>
2023-02-26 20:32:03 +0530

    Frequency in a range Brute force

---
+++ learn/Arrays/frequency_lim_range.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+        //-------- My approach --------//
+
+void fre(int arr[] , int n , int p){
+    sort(arr , arr+n);
+    int d = 0;
+    for(int i = 1 ; i<=p ; i++){
+        int c = 0;
+        for(int j = 0 ; j<n ; j++){
+            if(arr[j]%(n+1)==i){
+                c++;
+            }
+        }
+  
+        arr[i-1] = arr[i-1] + c*(n+1);
+    }
+
+    for(int i = 0 ; i<n ; i++){
+        cout<<arr[i]/(n+1)<<" ";
+    }
+}
+
+
+no mater whats the value of p we have to check for upto N no mater whats the value int the array we have to count the no of 
+times the elements are and modify the array
+
+*/
+
+//------------ Brute Force appraoch -------------//
+// void fr(int arr[] , int n ){
+//     int ans[n];
+//     for(int i = 1 ; i<=n ; i++){
+//         int c = 0;
+//         for(int j = 0 ; j<n ; j++){
+ 
+//             if(arr[j]==i){
+//                 c++;
+//             }
+            
+//         }
+//         ans[i-1] = c;
+//     }
+//     for(int i = 0 ; i<n ; i++){
+//         arr[i] = ans[i];
+//     }
+//     cout<<endl;
+// }
+
+
+
+
+int main(){
+    int arr[] = {2,3,2,7,3,5};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+
+
+
+
+
+
+    return 0;
+}

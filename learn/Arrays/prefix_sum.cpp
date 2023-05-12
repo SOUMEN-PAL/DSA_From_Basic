8f02a859ac1ab23882682e24aa5288ca5d68452c
SOUMEN-PAL <112452467+SOUMEN-PAL@users.noreply.github.com>
2023-05-12 21:20:13 +0530

    prefix sum efficient solution

--- learn/Arrays/prefix_sum.cpp
+++ learn/Arrays/prefix_sum.cpp
@@ -11,12 +11,34 @@ int get_sum(int arr[] , int a , int b){
 }
 
 
+// efficient solution //
+
+int get_Sum(int arr[] , int n, int l  , int r){
+
+    int pre[n];
+    int sum = 0;
+
+    for(int i = 0 ; i<n ; i++){
+        sum += arr[i];
+        pre[i] = sum;
+    }
+    if(l==0){
+        return pre[r];
+    }
+    else{
+        return pre[r]-pre[l-1];// l-1 because i wat the current at the left index and to get it i must 
+    }
+
+}
+
+
 
 
 int main(){
     int arr[] = {2,8,3,4,9,5};
+    int n = sizeof(arr)/sizeof(arr[0]);
 
-    cout<<get_sum(arr,0,4)<<endl;
+    cout<<get_Sum(arr ,n,1,4)<<endl;
 
 
 

c3c558b98d024d65d32956d540ad3b45aa7a452c
SOUMEN-PAL <112452467+SOUMEN-PAL@users.noreply.github.com>
2023-06-01 19:06:12 +0530

    Binary Search Reurssive solution

--- learn/Searching/Binary_search.cpp
+++ learn/Searching/Binary_search.cpp
@@ -24,6 +24,23 @@ int binary_search_itre(int arr[] , int n , int x){
     return -1;
 }
 
+// recurssive solution //
+int binary_search_recurr(int arr[] , int low , int high , int x){
+    int mid = (high+low)/2;
+    if(low>high){
+        return -1;
+    }
+    if(arr[mid]==x){
+        return mid;
+    }
+    if(arr[mid]>x){
+        return binary_search_recurr(arr , low , mid-1 , x);
+    }
+    else{
+        return binary_search_recurr(arr , mid+1 , high , x);
+    }
+}
+
 
 
 
@@ -31,12 +48,17 @@ int binary_search_itre(int arr[] , int n , int x){
 
 int main(){
     
-    int arr[] = {1,2,3,4,5,6,7};
+    int arr[] = {1,2,3,4,5,6,7,34,5,6,7,8,9,0,4,3,32,4,4,59,4,2,2,3,4,5,6,7,8,444,6,4,3,2,4};
     int n = sizeof(arr)/sizeof(arr[0]);
-    int x = 6;
+    int x = 9;
+    sort(arr , arr+n);
+    for(int i = 0 ; i<n ; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
 
     cout<<binary_search_itre(arr , n , x)<<endl;
-    cout<<(upper_bound(arr , arr+n , x)-arr)-1;
+    cout<<binary_search_recurr(arr , 0 , n-1 , x)<<endl;
 
 
 

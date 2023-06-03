4a70bcf6f679234d000d2653e06263ad20b3f83b
SOUMEN-PAL <112452467+SOUMEN-PAL@users.noreply.github.com>
2023-06-03 19:36:03 +0530

    Ternary search

---
+++ learn/Searching/Ternary_search.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//---------Ternary Search-----------//
+// int ternary_search(int arr , int low , int high , int mid1 , int mid2){
+//     int mid1 = low+(
+// }
+
+
+
+
+int main(){
+    
+    return 0;
+}

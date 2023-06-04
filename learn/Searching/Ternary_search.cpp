d0f8c3bd2075fd60c919dfc47701f29bc2e8d4e4
SOUMEN-PAL <112452467+SOUMEN-PAL@users.noreply.github.com>
2023-06-04 21:30:09 +0530

    Itreative solution for ternary search

--- learn/Searching/Ternary_search.cpp
+++ learn/Searching/Ternary_search.cpp
@@ -1,6 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
+// recursive solution //
 int ternarySearch(int l, int r, int key, int ar[]) 
 { 
     if (r >= l) 
@@ -47,6 +49,51 @@ int ternarySearch(int l, int r, int key, int ar[])
     return -1; 
 } 
 
+// itreative solution //
+
+int ternarysearch(int l, int r, int key, int ar[]) 
+{ 
+    while (r >= l) { 
+  
+        // Find mid1 and mid2 
+        int mid1 = l + (r - l) / 3; 
+        int mid2 = r - (r - l) / 3; 
+  
+        // Check if key is present at any mid 
+        if (ar[mid1] == key) { 
+            return mid1; 
+        } 
+        if (ar[mid2] == key) { 
+            return mid2; 
+        } 
+  
+        // Since key is not present at mid, 
+        // check in which region it is present 
+        // then repeat the Search operation 
+        // in that region 
+  
+        if (key < ar[mid1]) { 
+  
+            // The key lies in between l and mid1 
+            r = mid1 - 1; 
+        } 
+        else if (key > ar[mid2]) { 
+  
+            // The key lies in between mid2 and r 
+            l = mid2 + 1; 
+        } 
+        else { 
+  
+            // The key lies in between mid1 and mid2 
+            l = mid1 + 1; 
+            r = mid2 - 1; 
+        } 
+    } 
+  
+    // Key not found 
+    return -1; 
+} 
+
 
 
 int main(){
@@ -55,6 +102,7 @@ int main(){
     int arr[] = {1,2,3,4,5,6,7,8,99,100};
     int n = sizeof(arr)/sizeof(arr[0]);
     cout<<ternarySearch(0 , n-1 , 91 , arr)<<endl;
+    cout<<ternarysearch(0,n-1 , 100 , arr)<<endl;
     
     return 0;
 }

b6c6eaaa346482af4fb450ed88206efb3095a366
SOUMEN <soumenpal3241@gmail.com>
2023-06-17 18:42:10 +0530

    sorted rotated

---
+++ learn/Searching/Search_sorted_rotated_arr.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int Search_Rotated(int arr[] , int n , int x){
+
+    int low = 0 , high = n-1;
+    while(high>=low){
+        int mid = (high + low)/2;
+        if(arr[mid]==x){
+            return mid;
+        }
+        
+
+    }
+
+
+}
+
+
+int main(){
+
+
+    cout<<"hello";
+
+    return 0;
+}
+
+
+
+
+
+

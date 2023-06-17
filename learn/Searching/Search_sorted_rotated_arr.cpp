6ec4245bb2d30c738bcf3e86a46a9e1d91dff351
SOUMEN <soumenpal3241@gmail.com>
2023-06-17 23:34:47 +0530

    modified

--- learn/Searching/Search_sorted_rotated_arr.cpp
+++ learn/Searching/Search_sorted_rotated_arr.cpp
@@ -11,9 +11,24 @@ int Search_Rotated(int arr[] , int n , int x){
         if(arr[mid]==x){
             return mid;
         }
-        
-
+        else if(arr[low]<=arr[mid]){
+            if(x<arr[mid] && x>=arr[low]){
+                high = mid - 1;
+            }
+            else{
+                low = mid +1;
+            }
+        }
+        else{
+            if(x>arr[mid] && x<=arr[high]){
+                low = mid + 1;
+            }
+            else{
+                high = mid + 1;
+            }
+        }
     }
+    return -1;
 
 
 }
@@ -21,8 +36,10 @@ int Search_Rotated(int arr[] , int n , int x){
 
 int main(){
 
-
-    cout<<"hello";
+    int arr[] = {100,200,300,1,2,3,4,5,6,7,8};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int x = 2;
+    cout<<Search_Rotated(arr , n , x)<<endl;
 
     return 0;
 }

8437a4ed737d3b59d28e294b36e543fe791c0868
SOUMEN <soumenpal3241@gmail.com>
2023-06-21 22:20:57 +0530

    Updated subarray given sum and solved a edge case

--- learn/Arrays/subarray_given_sum.cpp
+++ learn/Arrays/subarray_given_sum.cpp
@@ -21,11 +21,12 @@ bool sub_arr_given_sum(int arr[] , int n , int sum){
     int s = 0 , curr = 0;
     for(int i = 0 ; i<n ; i++){
         curr+=arr[i];
-        while(curr>sum){
+        while(curr>sum && s<i){
             curr-=arr[s];
             s++;
         }
         if(curr==sum){
+            cout<<i<<" "<<s<<endl;
             return true;
         }
     }
@@ -37,7 +38,7 @@ bool sub_arr_given_sum(int arr[] , int n , int sum){
 int main(){
     int arr[] = {1,4,20,3,10,5};
     int n = sizeof(arr)/sizeof(arr[0]);
-    int sum = 33;
+    int sum = 0;
 
     bool ans = sub_arr_given_sum(arr , n , sum);
     if(ans){

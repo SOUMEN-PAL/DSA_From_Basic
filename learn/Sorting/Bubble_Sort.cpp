396d43ac16d3a1d3ec0a25cff33f8c8b6ea69353
SOUMEN <soumenpal3241@gmail.com>
2023-07-15 10:37:34 +0530

    Bubble sort

--- learn/Sorting/Bubble_Sort.cpp
+++ learn/Sorting/Bubble_Sort.cpp
@@ -25,12 +25,29 @@ void Bubble_Sort(int arr[] , int n){
 }
 
 
+// Efficient solution //
+void Sort_Bubble(int arr[] , int n){
+    for(int i = 0 ; i<n ; i++){
+        bool swaped = false;
+        for(int j = 0 ; j<n-i-1 ; j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j] , arr[j+1]);
+                swaped = true;
+            }
+        }
+        if(swaped == false){
+            break;
+        }
+    }
+}
+
+
 
 int main(){
     int arr[] = {9,8,7,6,5,4,3,2,1};
     int n = sizeof(arr)/sizeof(arr[0]);
 
-    Bubble_Sort(arr , n);
+    Sort_Bubble(arr , n);
     for(int i = 0 ; i<n ; i++){
         cout<<arr[i]<<" ";
     }

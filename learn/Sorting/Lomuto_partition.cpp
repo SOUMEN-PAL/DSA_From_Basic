5bd41cb234091a69bbdc5de1e40e7b8ef736cee3
Soumen <soumenpal3241@gmail.com>
2023-08-14 14:25:12 +0530

    Lomutos partition

--- learn/Sorting/Lomuto_partition.cpp
+++ learn/Sorting/Lomuto_partition.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 
 
-int lomuto_partition(int arr[] , int l , int h){
+int lomuto_partition(int arr[] , int l , int h , int p){
+    swap(arr[p] , arr[h]);
     int pivot = arr[h];
     int i = l-1;
     for(int j = l ; j<=h-1 ; j++){
@@ -11,7 +12,7 @@ int lomuto_partition(int arr[] , int l , int h){
             swap(arr[i] , arr[j]);
         }
     }
-    swap(arr[i+1] , arr[h]);
+    swap(arr[i+1] , arr[h]);// faltu ke kuch swaps jinki jarurat nahi ha//
     return i+1;
 }
 
@@ -20,7 +21,7 @@ int lomuto_partition(int arr[] , int l , int h){
 int main(){
     int arr[] = {5,12,74,10,5,7,6,2,6};
     int n = sizeof(arr)/sizeof(arr[0]);
-    int pivot = lomuto_partition(arr , 0 , n-1);
+    int pivot = lomuto_partition(arr , 0 , n-1 , 1);
     for(int i = 0 ; i<n ; i++){
         cout<<arr[i]<<" ";
     }

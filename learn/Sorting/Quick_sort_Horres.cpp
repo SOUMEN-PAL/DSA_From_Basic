63abe22ecbffd64592a42572983828fabce76a7d
Soumen <soumenpal3241@gmail.com>
2023-09-01 09:08:36 +0530

    Quick sort analysis

--- learn/Sorting/Quick_sort_Horres.cpp
+++ learn/Sorting/Quick_sort_Horres.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 int partition(int arr[], int l, int h)
 {
+    
+
     int pivot = arr[l];
     int i = l-1 , j = h+1;
     while(true){
@@ -24,16 +26,16 @@ void Q_sort(int arr[], int low, int high) {
     if (low < high)
     {
         int p = partition(arr, low, high);
-        cout<<p<<endl;
-        Q_sort(arr, low, p-1);
-        Q_sort(arr, p, high);
+        
+        Q_sort(arr, low, p);
+        Q_sort(arr, p+1, high);
     }
 }
 
 
 
 int main() {
-    int arr[] = {5,3,1,2,4};
+    int arr[] = {1,6,3,2,58,5,4,5};
     int n = sizeof(arr)/sizeof(arr[0]);
     Q_sort(arr , 0 , n-1);
     for(int i = 0 ; i<n ; i++){

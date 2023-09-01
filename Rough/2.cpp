42025163f12ecfc766ec609951b062525045ec30
Soumen <soumenpal3241@gmail.com>
2023-09-01 11:51:46 +0530

    three way paritioning

--- Rough/2.cpp
+++ Rough/2.cpp
@@ -1,9 +1,48 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+
+
+
+void threeWayPartition(vector<int>& array,int a, int b)
+{
+    int l = -1 , r = array.size();
+    while(true){
+        do{
+            l++;
+        }while(array[l]<a);
+        
+        do{
+            r--;
+        }while(array[r]>b);
+        if(l>=r){
+            break;
+        }
+        swap(array[l] , array[r]);
+    }
+    l = -1;
+    r = array.size();
+    while(true){
+        do{
+            l++;
+        }while(array[l]<b);
+        
+        do{
+            r--;
+        }while(array[r]>b);
+        if(l>=r){
+            break;
+        }
+        swap(array[l] , array[r]);
+    }
+}
+        
 
 int main(){
     
-    string x="Hello";
-    cout<<x.length();
+    vector<int> arr = {30 ,47 ,91 ,48 ,71 ,52 ,81 ,32 ,94 ,58 ,28 ,13 ,87 ,15 ,56 ,13 ,91 ,13 ,80};
+    threeWayPartition(arr , 11 , 30);
+    for(int i : arr){
+        cout<<i<<" ";
+    }
     return 0;
 }

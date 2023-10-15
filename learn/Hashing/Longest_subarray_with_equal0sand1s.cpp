c2505f7b37085dd72fec089fbbae28dafebabfc8
Soumen <soumenpal3241@gmail.com>
2023-10-15 15:01:49 +0530

    Subarrays with equal 0s and 1s in an binary arrray (cont and also finding the longest)

--- learn/Hashing/Longest_subarray_with_equal0sand1s.cpp
+++ learn/Hashing/Longest_subarray_with_equal0sand1s.cpp
@@ -23,11 +23,60 @@ int longest_1sand0s(int arr[] , int n){
     return res;
 }
 
+// Efficient soltion for no of subarrays with equal no of zeroes and ones//
+int  NumberOFSubarray(int arr[] , int n){
+    for(int i = 0 ; i<n ; i++){
+        if(arr[i] == 0){
+            arr[i] = -1; //____+modifying the array to make it look like a zero sum subarray //
+        }
+    }
+
+    int Pre_sum = 0;
+    int count = 0;
+    unordered_map<int , int> mp; // for storing the number of places the prfix sum is same representing that the subarray is 0 //
+    for(int i = 0 ; i<n ; i++){
+        Pre_sum += arr[i];
+        if(Pre_sum == 0){
+            count++;
+        }
+        if(mp.find(Pre_sum) != mp.end()){
+            count += mp[Pre_sum];
+        }
+        mp[Pre_sum]++;
+    }
+    return count;
+}
+
+int longestArray(int arr[] , int n){
+    for(int i = 0 ; i<n ; i++){
+        if(arr[i] == 0){
+            arr[i] = -1; //____+modifying the array to make it look like a zero sum subarray //
+        }
+    }
+    int curr = 0;
+    int res = 0;
+    unordered_map<int , int> mp;
+    for(int i = 0 ; i<n ; i++){
+        curr += arr[i];
+        if(curr == 0){
+            res = i+1;
+        }
+        if(mp.find(curr) == mp.end()){
+            mp.insert({curr  , i});
+        }
+        if(mp.find(curr) != mp.end()){
+            res = max(res , i - mp[curr]);
+        }
+    }
+    return res;
+}
 
 int main(){
     int arr[] = {1,0,1,1,1,0,0};
     int n = sizeof(arr)/sizeof(arr[0]);
     cout<<longest_1sand0s(arr , n)<<endl;
+    cout<<longestArray(arr , n)<<endl;
+    cout<<NumberOFSubarray(arr , n)<<endl;
 
     return 0;
 }

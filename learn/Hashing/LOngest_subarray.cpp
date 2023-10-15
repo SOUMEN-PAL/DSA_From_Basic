34a38ede233a1af2d11588eec11e0501302cb013
Soumen <soumenpal3241@gmail.com>
2023-10-15 11:58:10 +0530

    Longest subarray with given sum including integer values

--- learn/Hashing/LOngest_subarray.cpp
+++ learn/Hashing/LOngest_subarray.cpp
@@ -16,11 +16,31 @@ int longest_sub(int arr[] , int n , int sum){
     }
     return res;
 }
+// Efficient solution //
+int longestSUbarraySum(int arr[] , int n , int sum){
+    int res = 0;
+    int curr = 0;
+    unordered_map<int , int> mp;
+    for(int i = 0 ; i<n ; i++){
+        curr += arr[i];
+        if(curr == sum){
+            res = max(res , i+1); // checking for 0 index subarray with curr == sum //
+        }
+        if(mp.find(curr) == mp.end()){
+            mp.insert({curr , i}); // adding unknown element only once //
+        }
+        if(mp.find(curr - sum) != mp.end()){
+            res = max(res , i - mp[curr - sum]); // Finding the other subarray //
+        }
+    }
+    return res;
+}
 
 int main(){
     int arr[]={5,8,-4,-4,9,-2 , 2};
     int n = sizeof(arr)/sizeof(arr[0]);
     int sum = 0;
     cout<<longest_sub(arr , n , sum)<<endl;
+    cout<<longestSUbarraySum(arr , n , sum)<<endl;
     return 0;
 }

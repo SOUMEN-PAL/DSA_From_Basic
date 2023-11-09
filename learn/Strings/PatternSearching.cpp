986f44f824da51979f2bb8d35e79fe3d719eb377
Soumen <soumenpal3241@gmail.com>
2023-11-09 23:29:45 +0530

    Rabin KArp solution

--- learn/Strings/PatternSearching.cpp
+++ learn/Strings/PatternSearching.cpp
@@ -41,8 +41,45 @@ void naiveDistinctPattern(string txt , string patr){
 }
 
 // TODO : Rabin Karp Algorithm //
+int d = 256;
+int q = 101;
+void RBkarp(string txt , string pat){
+    int n = txt.length();
+    int m = pat.length();
+    int h = 1;
+    for(int i = 0 ; i < m - 1 ; i++){
+        h = (h * d) % q;
+    }
+    int p = 0 , t = 0;
+    for(int i = 0 ; i < m ; i++){
+        p = (p * d + pat[i]) % q;
+        t = (t * d + txt[i]) % q;
+    }
+
+    for(int i = 0 ; i<= n-m ; i++){
+        if(p == t){
+            bool flag = true;
+            for(int j = 0 ; j < m ; j++){
+                if(txt[i + j] != pat[j]){
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag){}
+                cout << i << " ";
+            }
+        if(i < n - m){
+            t = (d * (t - txt[i] * h) + txt[i + m]) % q;
+            if(t < 0)
+                t = t + q;
+        }
+    }
+}
+
 
 int main(){
-    
+    string s = "AABAACAADAABAABA";
+    string p = "AABA";
+    RBkarp(s , p);
     return 0;
 }

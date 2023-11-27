2e2d4ac504a5be5c5685b3b18c257eb9d5556260
Soumen <soumenpal3241@gmail.com>
2023-11-27 15:18:05 +0530

    Fill LPs array efficient solution

--- learn/Strings/KMP_Fill_LPS.cpp
+++ learn/Strings/KMP_Fill_LPS.cpp
@@ -27,22 +27,47 @@ void fillProperPrefixSuffix(string s , int n , int lps[]){
 
 
 // Efficient Proper Prefix Suffix TC : O(n) //
-// void fillLps(string s , int arr[]){
-//     int n = s.length() , len = 0;
-//     int i = 1;
-    
-// }
+void fillLps(string str , int n , int lps[]){
+    lps[0] = 0;
+    int len = 0;
+    int i = 1;
+    while(i<n){
+        if(str[i] == str[len]){
+            len++;
+            lps[i] = len;
+            i++;
+        }
+        else{
+            if(len == 0){
+                lps[i] = 0;
+                i++;
+            }
+            else{
+                len = lps[len - 1];
+            }
+        }
+    }
+}
+
 
 
 int main(){
 
 
-    string s = "abbabb";
+    string s = "abacabad";
     int arr[s.length()];
     fillProperPrefixSuffix(s , s.length() , arr);
     for(int i = 0 ; i<s.length() ; i++){
         cout<<arr[i]<<" ";
     }
+    cout<<endl;
+    int arr1[s.length()];
+    fillLps(s , s.length() , arr1);
+    for(int i = 0 ; i<s.length() ; i++){
+        cout<<arr1[i]<<" ";
+    }
+    cout<<endl;
+    
     
     return 0;
 }

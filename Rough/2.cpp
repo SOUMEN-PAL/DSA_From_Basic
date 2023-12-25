afd6f7a0f04931aaf5e7ccb204ca251e283c41de
Souemen <soumenpal3241@gmail.com>
2023-12-25 22:32:26 +0530

    Linked list started

--- Rough/2.cpp
+++ Rough/2.cpp
@@ -1,59 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-string smallestWindow(string s, string p) {
-    int n = s.length();
-    int m = p.length();
-
-    if (n < m) {
-        return "-1";
+class Node{
+    public:
+        int data;
+        Node* next;
+    
+    Node(int data){
+        this->data = data;
+        next = NULL;
     }
-
-    unordered_map<char, int> s_map;
-    unordered_map<char, int> p_map;
-
-    for (int i = 0; i < m; i++) {
-        p_map[p[i]]++; // Initialize the values of the pattern
-    }
-
-    int curr_len = 0;
-    string res = "-1";
-    int res_len = INT_MAX;
-    int l = 0, r = 0;
-
-    while (r < n) {
-        s_map[s[r]]++;
-
-        if (p_map.find(s[r]) != p_map.end() && p_map[s[r]] == s_map[s[r]]) {
-            curr_len++;
-        }
-
-        while (curr_len == p_map.size()) {
-            if (r - l + 1 < res_len) {
-                res = s.substr(l, r - l + 1);
-                res_len = r - l + 1;
-            }
-            s_map[s[l]]--;
-
-            if (p_map.find(s[l]) != p_map.end() && p_map[s[l]] == s_map[s[l]]) {
-                curr_len--;
-            }
-            l++;
-        }
-        r++;
-    }
-
-    if (res_len == INT_MAX) {
-        return "-1";
-    }
-    return res;
-}
+};
 
 
 int main(){
-    string s = "timetopractice";
-    string p = "tttoooccoc";
-    cout<<smallestWindow(s , p)<<endl;
-    cout<<s.substr(0, 3+1);
+    
+    Node* head = new Node(10);
+
+    cout<<head->data<<endl;
+
+
     return 0;
 }
---
+++ learn/Linked List/implementation_linked_list.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int data = NULL;
+        Node*next;
+
+        Node(int val){
+            data = val;
+            next = NULL;
+        }
+};
+
+void traverse(Node* head){
+    if(head == NULL || head->data == NULL){
+        cout<<"Linked List is empty"<<endl;
+        return;
+    }
+    while(head != NULL){
+        cout<<head->data<<" ";
+        head = head->next;
+    }
+    cout<<endl;
+}
+
+int main(){
+    
+    Node*head = new Node(1);
+    Node* temp1 = new Node(2);
+    Node* temp2 = new Node(3);
+    Node* temp3 = new Node(4);
+    Node* temp4 = new Node(5);
+    Node* temp5 = new Node(6);
+    Node* temp6 = new Node(7);
+    Node* temp7 = new Node(8);
+    Node* temp8 = new Node(9);
+    Node* temp9 = new Node(10);
+    Node* temp10 = new Node(11);
+
+    head->next = temp1;
+    temp1->next = temp2;
+    temp2->next = temp3;
+    temp3->next = temp4;
+    temp4->next = temp5;
+    temp5->next = temp6;
+    temp6->next = temp7;
+    temp7->next = temp8;
+    temp8->next = temp9;
+    temp9->next = temp10;
+
+    Node *head1 = new Node(NULL);
+    traverse(head);
+    traverse(head1);
+
+
+    return 0;
+}

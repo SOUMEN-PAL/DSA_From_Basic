e5b1d3230aac33c06eb1f4cfe9a1acb22fa87e6c
Soumen Pal <soumenpal3241@gmail.com>
2024-02-19 14:49:26 +0530

    LargestAreaInHistogramBetter approach

--- Rough/2.cpp
+++ Rough/2.cpp
@@ -1,67 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Node {
-    public:
-        int data;
-        Node *next;
-        Node(int d){
-            data = d;
-            next = NULL;
-        }
-
-};
-
-void Traverse(Node *head){
-    Node *temp = head;
-    while(temp != NULL){
-        cout<<temp->data<<" ";
-        temp = temp->next;
-    }
-    cout<<endl;
-}
-
-Node *reverse(Node *head){
-    Node *prev = NULL;
-    Node *curr = head;
-    Node *next = NULL;
-    while(curr != NULL){
-        next = curr->next;
-        curr->next = prev;
-        prev = curr;
-        curr = next;
-    }
-    return prev;
-}
-
-Node *removeStartingZeroes(Node *head){
-    Node *curr = head;
-    while(curr != NULL && curr->data == 0){
-        curr = curr->next;
-    }
-    return curr;
-}
-
 int main(){
-    Node *head = new Node(0);
-    Node *temp1 = new Node(0);
-    Node *temp2 = new Node(0);
-    Node *temp3 = new Node(4);
-    Node *temp4 = new Node(5);
-    Node *temp5 = new Node(6);
-
-    head->next = temp1;
-    temp1->next = temp2;
-    temp2->next = temp3;
-    temp3->next = temp4;
-    temp4->next = temp5;
-
-    Traverse(head);
-    head = removeStartingZeroes(head);
-    Traverse(head);
-    head = reverse(head);
-    Traverse(head);
-    
-        
+    cout<<'z' - 'a';
     return 0;
 }
---
+++ learn/Stack/largestAreaInHistoGram.cpp
@@ -0,0 +1,100 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// Naive SOlution //
+int largestAreaInHistogram(int arr[] , int n){
+    int res = INT_MIN;
+
+    for(int i = 0 ; i<n ; i++){
+        int curr = arr[i];
+
+        for(int j = i-1  ; j>=0 ; j--){
+            if(arr[j]>=arr[i]){
+                curr = curr + arr[i];
+            }
+            else{
+                break;
+            }
+        }
+
+        for(int j = i + 1 ; j<n ; j++){
+            if(arr[j]>=arr[i]){
+                curr = curr + arr[i];
+            }
+            else{
+                break;
+            }
+        }
+
+        res = max(res , curr);
+    }
+    return res;
+}
+
+// Better Solution O(n) : TC//
+// we need the indices //
+//_________________PreProcessing Functions________________//
+vector<int> previousSmaller(int arr[] , int n){
+    stack<int> s;
+    vector<int> res;
+    s.push(0);
+    for(int i = 0 ; i<n ; i++){
+        while(s.empty() == false && arr[s.top()]>=arr[i]){
+            s.pop();
+        }
+
+        int ps = s.empty()?-1 : s.top();
+        res.push_back(ps);
+        s.push(i);
+    }
+    return res;
+
+}
+
+//_________________PreProcessing Functions________________//
+vector<int> nextsmaller(int arr[] , int n){
+    stack<int> s;
+    vector<int> res;
+    s.push(n-1);
+    for(int i = n-1 ; i>=0 ; i--){
+        while(s.empty() == false && arr[s.top()]>=arr[i]){
+            s.pop();
+        }
+
+        int ns = s.empty() ? n : s.top();
+        res.push_back(ns);
+        s.push(i);// isko //
+    }
+    reverse(res.begin() , res.end());
+    return res;
+}
+
+int largestAreaInHistogramBetter(int arr[] , int n){
+    vector<int> ns = nextsmaller(arr , n);
+    vector<int> ps = previousSmaller(arr , n);
+
+    int res = 0;
+    for(int i = 0 ; i<n ; i++){
+        int curr = arr[i];
+        curr = curr + ((i - ps[i] - 1)*arr[i]);
+        curr = curr + ((ns[i] - i - 1)*arr[i]);
+        res = max(res , curr);
+    }
+    return res;
+}
+
+int main(){
+    int arr[] = {6,2,5,4,1,5,6};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    cout<<largestAreaInHistogramBetter(arr , n)<<endl;
+    
+    
+    
+    // vector<int> res = nextsmaller(arr , n);
+    // for(auto i : res){
+    //     cout<<i<<" ";
+    // }
+    
+    return 0;
+}

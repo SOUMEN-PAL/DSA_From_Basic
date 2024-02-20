8cef347dcd7d11e73755fa28a44e8bd97e8b745b
Soumen Pal <soumenpal3241@gmail.com>
2024-02-20 20:12:14 +0530

    Largest area efficient

--- learn/Stack/largestAreaInHistoGram.cpp
+++ learn/Stack/largestAreaInHistoGram.cpp
@@ -84,11 +84,37 @@ int largestAreaInHistogramBetter(int arr[] , int n){
     return res;
 }
 
+
+//______________________________________Efficient Solution______________________________________//
+int LargestAreaHistogram(int arr[] , int n){
+    stack<int> s;
+    int res = 0;
+    for(int i = 0 ; i<n ; i++){
+        while(s.empty() == false && arr[s.top()]>=arr[i]){
+            int tp = s.top();
+            s.pop();
+            int curr = arr[tp]*(s.empty()?i : (i - s.top() - 1));
+            res = max(res , curr);
+        }
+        s.push(i);
+    }
+
+    while(s.empty() == false){
+        int tp = s.top();
+        s.pop();
+        int curr = arr[tp]*(s.empty()?n : (n - s.top() - 1));
+        res = max(res , curr);
+    }
+    return res;
+}
+
+
 int main(){
     int arr[] = {6,2,5,4,1,5,6};
     int n = sizeof(arr)/sizeof(arr[0]);
+    cout<<largestAreaInHistogram(arr , n)<<endl;
     cout<<largestAreaInHistogramBetter(arr , n)<<endl;
-    
+    cout<<LargestAreaHistogram(arr , n)<<endl;
     
     
     // vector<int> res = nextsmaller(arr , n);

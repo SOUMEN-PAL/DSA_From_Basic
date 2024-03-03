bf8a9b77dd4af72e5ab0c5569054693535df8e78
Soumen Pal <soumenpal3241@gmail.com>
2024-03-03 20:44:52 +0530

    Infix to postFix

---
+++ learn/Stack/infixPostandPre.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int prec(char ch){
+    if(ch == '^'){
+        return 3;
+    }
+    else if(ch == '/' || ch == '*'){
+        return 2;
+    }
+    else if(ch == '+' || ch == '-'){
+        return 1;
+    }
+    else{
+        return -1;
+    }
+}
+
+string infixToPostFix(string s){
+    int n = s.length();
+    stack<char> st;
+    string res = "";
+    for(int i = 0 ; i<n ; i++){
+        int x = s[i];
+        if((x>='a' && x<='z') || (x>='A' && x<='Z') || (x>='0' && x<='9')){
+            res.push_back(x);
+        }
+        else if(x == '('){
+            st.push(x);
+        }
+        else if(x == ')'){
+            while(st.top() != '('){
+                res.push_back(st.top());
+                st.pop();
+            }
+            st.pop(); // we do not need that ( for the future checks //
+        }
+        else{
+            while(!st.empty() && prec(x)<=prec(st.top())){
+                // abhi toh left to right ho ha equals me par agar kuch alag hua toh uske liye ek pura prec table chaiye //
+                res.push_back(st.top());
+                st.pop();
+            }
+            st.push(x);
+        }
+    }
+    while(st.empty()==false){
+        res.push_back(st.top());
+        st.pop();
+    }
+    return res;
+}
+
+
+int main(){
+    
+    string str = "a-b*c+g";
+    cout<<infixToPostFix(str)<<endl;
+
+
+    return 0;
+}

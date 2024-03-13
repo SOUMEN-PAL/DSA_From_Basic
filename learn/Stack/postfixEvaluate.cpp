54fdc4dcfadcc4c61256f93c5e10d1b9cfb1e99a
Soumen Pal <soumenpal3241@gmail.com>
2024-03-13 17:23:16 +0530

    evaluation of post fix:

---
+++ learn/Stack/postfixEvaluate.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// POst fix to infix //
+int evalPostFix(string s){
+    stack<int> st;
+    int n = s.length();
+    for(int i = 0 ; i<n ; i++){
+        char x = s[i];
+        if(x == ' '){
+            continue;
+        }
+
+        else if(isdigit(x) == true){
+            int res = 0;
+            while(isdigit(s[i])){
+                res = (res*10) + (s[i]-'0');
+                i++;
+            }
+            i--;
+            st.push(res);
+        }
+        else{
+            int op1 = st.top();st.pop();
+            int op2 = st.top();st.pop();
+            int res;
+            switch (x)
+            {
+            case '+':
+                res = op1 + op2;
+                break;
+            case '-':
+                res = op2 - op1;
+                break;
+            case '/':
+                res = op2 / op1;
+                break;
+            case '*':
+                res = op2*op1;
+                break;
+            case '^':
+                res = pow(op2 , op1);
+                break;
+            default:
+                break;
+            }
+            st.push(res);
+        }
+    }
+
+
+    return st.top();
+}
+
+int main(){
+    string s = "2 2 2 ^ ^";
+    cout<<evalPostFix(s);
+    return 0;
+}

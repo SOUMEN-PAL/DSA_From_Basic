ac9ea9bb4e78aeaf49045fa8608b6466fc2e72f4
Soumen Pal <soumenpal3241@gmail.com>
2024-03-20 20:08:01 +0530

    Most efficient solution

--- learn/Stack/Max_of_min_of_everySubArray.cpp
+++ learn/Stack/Max_of_min_of_everySubArray.cpp
@@ -67,9 +67,36 @@ vector<int> max_min_sub(vector<int> arr){
 }
 
 
+// Most Efficient Solution //
+vector<int> maxOfMinOfEverySizeSubarray(vector<int> arr){
+    int n = arr.size();
+    vector<int> res(n , 0);
+    stack<int> s;
+    for(int i = 0 ; i<n ; i++){
+        while(s.empty() == false && arr[i]<=arr[s.top()]){
+            int index = s.top();
+            s.pop();
+            int places = s.empty()?i:i-s.top()-1;
+            res[places-1] = max(res[places-1] , arr[index]);
+        }
+        s.push(i);
+    }
+    while(s.empty() == false){
+        int index = s.top();
+        s.pop();
+        int places = s.empty()?n:n-s.top()-1;
+        res[places-1] = max(res[places-1] , arr[index]);
+    }
+
+    for(int i = n-2 ; i>=0 ; i--){
+        res[i] = max(res[i] , res[i+1]);
+    }
+    return res;
+}
+
 int main(){
-    vector<int> arr = {10,20,15,50,10,70,30};
-    vector<int> ans = max_min_sub(arr);
+    vector<int> arr = {10,20,30,50,10,70,30};
+    vector<int> ans = maxOfMinOfEverySizeSubarray(arr);
     for(auto i: ans){
         cout<<i<<" ";
     }

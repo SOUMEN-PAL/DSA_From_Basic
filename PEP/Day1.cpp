63bfd121619dac00d0afc28e6584e0192a08a4de
Soumen Pal <soumenpal3241@gmail.com>
2024-06-06 10:47:02 +0530

    Added the PEP class

---
+++ PEP/Day1.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Observation of the problem 
+// Checking for the brute force approach
+// what is the problem in the brute force what is increasing the time complexity
+// Remove that problem
+
+
+
+int main(){
+    
+    return 0;
+}

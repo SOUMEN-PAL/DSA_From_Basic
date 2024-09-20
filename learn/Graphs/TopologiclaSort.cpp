52e5d7b8b4cfabf9b68fd606da49aa08e8f81302
Soumen <soumenpal3241@gmail.com>
2024-09-20 23:42:23 +0530

    contd after project done

--- learn/Graphs/TopologiclaSort.cpp
+++ learn/Graphs/TopologiclaSort.cpp
@@ -18,6 +18,8 @@ void printList(vector<int> adj[] , int v){
 }
 
 
+
+//Topo sort using the bfs and and the kans algo
 vector<int> TopoSort(vector<int> adj[] , int v){
     vector<int> in(v , 0);
     for(int i = 0 ; i<v ; i++){
@@ -53,6 +55,39 @@ vector<int> TopoSort(vector<int> adj[] , int v){
 
 
 
+void DFS(vector<int> adj[], int u,stack<int> &st, bool visited[]) 
+{ 	
+    visited[u]=true;
+    
+    for(int v:adj[u]){
+        if(visited[v]==false)
+            DFS(adj,v,st,visited);
+    }
+    st.push(u);
+}
+
+
+void topologicalSort(vector<int> adj[], int V) 
+{ 
+    bool visited[V]; 
+	for(int i = 0;i<V; i++) 
+		visited[i] = false;
+	stack<int> st;
+    
+    for(int u=0;u<V;u++){
+        if(visited[u]==false){
+            DFS(adj,u,st,visited);
+        }
+    }
+    
+    while(st.empty()==false){
+        int u=st.top();
+        st.pop();
+        cout<<u<<" ";
+    }
+
+}
+
 
 int main(){
     int v = 5;
@@ -68,6 +103,11 @@ int main(){
     for(auto i : res){
         cout<<i<<" ";
     }
+
+    cout<<endl;
+
+    topologicalSort(adj , v);
+
 
     return 0;
 }

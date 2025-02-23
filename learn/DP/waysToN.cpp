e7c2ebf1ba3bd51750bf28f87ef0d8ea26cd6cc1
Soumen <soumenpal3241@gmail.com>
2025-02-23 22:53:22 +0530

    Ways to n dp solution

---
+++ learn/DP/waysToN.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int MOD = 1e9 + 7;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> dp(n+1, 0);
+    dp[0] = 1; // base case: one way to partition 0
+    
+    // iterate over possible parts (1 to n-1, because n itself is not allowed)
+    for(int i = 1; i <= n-1; i++){
+        for(int j = i; j <= n; j++){
+            dp[j] = (dp[j] + dp[j-i]) % MOD;
+        }
+    }
+    cout << dp[n] % MOD;
+    return 0;
+}

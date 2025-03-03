a31e4df2a05743f0969e8ce8f3699b1e5bf21b12
Soumen Pal <soumenpal3241@gmail.com>
2025-03-03 19:30:25 +0530

    kadane with dp

--- learn/Arrays/max_subarray_sum.cpp
+++ learn/Arrays/max_subarray_sum.cpp
@@ -1,48 +1,45 @@
 #include <bits/stdc++.h>
+#include <vector>
 using namespace std;
-
 
 // naive approach //
 
+int max_sum(vector<int> arr, int n) {
+  int res = arr[0]; // muje yaha pata ha array ka subarray start hamesa 0 th
+                    // element se hi hoga //
 
-int max_sum(int arr[] , int n){
-    int res = arr[0]; // muje yaha pata ha array ka subarray start hamesa 0 th element se hi hoga //
-
-    for(int i = 0 ; i<n ; i++){
-        int curr = 0;
-        for(int j  = i ; j<n ; j++){
-            curr = arr[j]+curr;
-            res = max(res , curr);
-        }
+  for (int i = 0; i < n; i++) {
+    int curr = 0;
+    for (int j = i; j < n; j++) {
+      curr = arr[j] + curr;
+      res = max(res, curr);
     }
-    return res;
-
+  }
+  return res;
 }
 
 // efficient solution //
 // kadane algo //
- int mSum(int arr[] , int n){
-    int res = arr[0];
-    int maxend = arr[0];
-    for(int i = 1 ; i<n ; i++ ){
-        maxend = max(arr[i] , maxend+arr[i]);
-        res = max(res , maxend);
-    }
-    return res;
+int mSum(vector<int> arr, int n) {
+  int res = arr[0];
+  int maxend = arr[0];
+  for (int i = 1; i < n; i++) {
+    maxend = max(arr[i], maxend + arr[i]);
+    res = max(res, maxend);
+  }
+  return res;
+}
 
- }
+int main() {
 
+  int n;
+  cin >> n;
+  vector<int> arr(n);
+  for (int i = 0; i < n; i++) {
+    cin >> arr[i];
+  }
+  cout << max_sum(arr, n) << endl;
+  cout << mSum(arr, n) << endl;
 
-
-
-
-
-int main(){
-    int arr[] = {-2,1,-3,-5,4,-1,2,1,-4,1};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    cout<<max_sum(arr , n)<<endl;
-    cout<<mSum(arr , n)<<endl;
-
-
-    return 0;
+  return 0;
 }

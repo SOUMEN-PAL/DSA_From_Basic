b17240eca9eb849165aea93bbc2fa3797cc69be0
Soumen Pal <soumenpal3241@gmail.com>
2025-04-07 22:32:43 +0530

    lis done

--- learn/DP/lis.cpp
+++ learn/DP/lis.cpp
@@ -1,17 +1,51 @@
 #include<bits/stdc++.h>
+#include <cmath>
 using namespace std;
 typedef long long ll;
 const ll INF = 1LL << 60;
 
 //n log n solution
-int ceil(vector<int> tail){
+int upperBound(vector<int> tail , int target){
+    int low = 0 , high = tail.size();
 
+    while(low < high){
+        int mid = low + (high - low)/2;
 
+        if(tail[mid] < target){
+            low = mid + 1;
+        }else{
+            high = mid;
+        }
+    }
+    return low;
+}
+int lengthOfLIS(vector<int>& arr) {
+    vector<int> tail;
+    int n = arr.size();
+    for(int i = 0 ; i<n ; i++){
+        if (tail.empty() || arr[i] > tail.back()) {
+            tail.push_back(arr[i]);
+        } else {
+            int index = upperBound(tail, arr[i]);
+            tail[index] = arr[i];
+        }
+    }
+    return tail.size();
 }
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for(int i = 0 ; i<n ; i++){
+        cin>>arr[i];
+    }
+    
+    int ans = sol(arr , n);
+    cout<<ans<<endl;
     
     return 0;
 }
